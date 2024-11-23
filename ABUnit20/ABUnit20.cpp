4aef315d6e648f89b19ea0602fb12482feec3ae8
IvanDev <123570888+IvanDev-ai@users.noreply.github.com>
2024-11-23 16:50:12 +0100

    Add project files.

---
+++ ABUnit20/ABUnit20.cpp
@@ -0,0 +1,12 @@
+﻿// ABUnit20.cpp : Defines the entry point for the application.
+//
+
+#include "ABUnit20.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
